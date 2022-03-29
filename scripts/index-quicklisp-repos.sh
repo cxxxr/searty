5274ac01237409373cdc2cb2a1ffcc4e392d4f1c
cxxxr <g23tlm@gmail.com>
2022-03-29 23:33:47 +0900

    add

---
+++ scripts/index-quicklisp-repos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ROOT_DIR=$1
+
+sqlite3 /tmp/searty.sqlite3 < ../schema.sql
+
+for dir in $(ls -1 $ROOT_DIR)
+do
+    repo=$ROOT_DIR/$dir
+    ./searty-index.ros $repo
+    if [ $? -ne 0 ]
+    then
+        echo $repo >> failure.txt
+    fi
+done

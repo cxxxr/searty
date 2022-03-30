f3451481f47e7dfeb74bb1c267906485b7f4867b
cxxxr <g23tlm@gmail.com>
2022-03-30 22:50:26 +0900

    fix

--- scripts/index-quicklisp-repos.sh
+++ scripts/index-quicklisp-repos.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+    echo 'usage: $0 directory'
+fi
+
 ROOT_DIR=${1%/}
 
 rm ../index/*
@@ -8,7 +13,7 @@ sqlite3 ../index/searty.sqlite3 < ../schema.sqlite3
 for dir in $(ls -1 $ROOT_DIR)
 do
     repo=$ROOT_DIR/$dir
-    ./searty-index.ros $repo
+    ./searty-index $repo
     if [ $? -ne 0 ]
     then
         echo $repo >> failure.txt
--- sqlite3-database.lisp
+++ sqlite3-database.lisp
@@ -18,7 +18,7 @@
 (defmethod upsert-inverted-index ((database sqlite3-database) token-id locations)
   (let* ((encoded-locations (encode-locations-to-vector locations))
          (filepath (merge-pathnames token-id *index-directory*)))
-    (write-byte-vector-into-file encoded-locations filepath)
+    (write-byte-vector-into-file encoded-locations filepath :if-exists :supersede)
     (execute-sql (database-connection database)
                  "INSERT INTO inverted_index (token_id, locations_data_file) VALUES (?, ?)
 ON CONFLICT(token_id) DO NOTHING"

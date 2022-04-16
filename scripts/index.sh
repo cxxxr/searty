b04972e1f04a7228efb61b24ef8891ffc1dd7545
cxxxr <g23tlm@gmail.com>
2022-04-16 17:00:51 +0900

    update script

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ./gen-makefile.ros ~/quicklisp-dist/systems.txt ~/quicklisp-dist/2022-04-01/ $(realpath ../index/) Makefile
-rm -rf success.txt failure.txt ../index/
+rm -rf ../index/
 time make -j24 -k 2>&1 | tee log
+time ./searty-merge.ros $(realpath ../index/) searty.db

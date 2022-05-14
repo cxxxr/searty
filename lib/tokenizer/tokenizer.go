e1b0e5b23a3bd7f4eff1b74e3f116b8312e05981
cxxxr <g23tlm@gmail.com>
2022-05-14 16:52:05 +0900

    byte単位でなくrune単位でトークンにする

--- lib/tokenizer/tokenizer.go
+++ lib/tokenizer/tokenizer.go
@@ -8,18 +8,26 @@ func New() *Tokenizer {
 
 func ngram(text string, n int) []string {
 	tokens := make([]string, 0)
-	for i := 0; i < len(text)-(n-1); i++ {
-		tokens = append(tokens, text[i:i+n])
+
+	for i := range text {
+		token := make([]rune, 0, 3)
+		limit := n
+		for _, c := range text[i:] {
+			token = append(token, c)
+			limit--
+			if limit == 0 {
+				break
+			}
+		}
+		if len(token) < n {
+			break
+		}
+		tokens = append(tokens, string(token))
 	}
+
 	return tokens
 }
 
 func (t *Tokenizer) Tokenize(text string) []string {
-	// boundingText := make([]byte, len(text)+2)
-	// copy(boundingText[1:], text)
-	// boundingText[0] = byte(0)
-	// boundingText[len(boundingText)-1] = byte(0)
-	// return ngram(string(boundingText), 3)
-
 	return ngram(string(text), 3)
 }

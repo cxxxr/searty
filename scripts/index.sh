2b8721c261840786e94b7017dc61f519947e450e
cxxxr <g23tlm@gmail.com>
2022-05-21 14:43:39 +0900

    wip

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ros build searty-index.ros
-
 ./gen-makefile.ros ~/quicklisp-dist/systems.txt ~/quicklisp-dist/2022-04-01/ $(realpath ../build/) $(realpath ../index/) Makefile
 
 rm -rf ../build ../index
@@ -9,8 +7,12 @@ mkdir ../build ../index
 
 make build -j24 -k 2>&1 | tee make.log
 
-ls ../build/*.out | parallel ./searty-index.sh
+cd ../cmd/searty-index
+go build
+ls ../build/*.out | parallel ./searty-index -o ../../index
+cd -
 
 rm -rf ../db
 mkdir ../db
-./searty-merge.ros $(realpath ../index) ../db/quicklisp.db
+cd ../cmd/searty-merge
+go run . -o ../db/quicklisp.db ../index/*.sqlite3

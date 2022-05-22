23a05498bd2e15832b559e606b7fd8670be2218d
cxxxr <g23tlm@gmail.com>
2022-05-22 18:00:37 +0900

    fix

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-./gen-makefile.ros ~/quicklisp-dist/systems.txt ~/quicklisp-dist/2022-04-01/ $(realpath ../build/) $(realpath ../index/) Makefile
+./gen-makefile.ros ~/quicklisp-dist/systems.txt ~/quicklisp-dist/2022-04-01/ $(realpath ../build/analyze/) $(realpath ../build/db/) Makefile
 
-rm -rf ../build ../index
-mkdir ../build ../index
+rm -rf ../build
+mkdir ../build
+mkdir ../build/analyze ../build/db
 
-make build -j24 -k 2>&1 | tee make.log
+make analyze -j24 -k
 
 cd ../cmd/searty-index
 go build
-ls ../build/*.out | parallel ./searty-index -o ../../index
+ls ../../build/analyze/*.json | parallel ./searty-index -o ../../build/db
 cd -
 
 rm -rf ../db
 mkdir ../db
 cd ../cmd/searty-merge
-go run . -o ../db/quicklisp.db ../index/*.sqlite3
+go run . -o ../../db/quicklisp.db ../../build/db/*.sqlite3
